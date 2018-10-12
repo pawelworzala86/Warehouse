99a5b25d91d4ac7ec39933e47ad3021d57686357
worzala <worzala86@gmail.com>
2018-10-12 20:00:27 +0200

    poprawki responsywności

--- Gen/grid.php
+++ Gen/grid.php
@@ -7,6 +7,7 @@ define('TAB', "\t");
 $grid = '.row {' . PHP_EOL .
     TAB . 'margin-left: -10px;' . PHP_EOL .
     TAB . 'margin-right: -10px;' . PHP_EOL .
+    TAB . 'display: block;' . PHP_EOL .
     '}' . PHP_EOL;
 
 $grid .= '.col {' . PHP_EOL .
@@ -19,7 +20,7 @@ $grid .= '.col {' . PHP_EOL .
     '}' . PHP_EOL;
 
 //.table .col
-for ($i = 0; $i < GRID_X; $i++) {
+/*for ($i = 0; $i < GRID_X; $i++) {
     $left = '';
     if ($i == 0) {
         $left = 0;
@@ -31,7 +32,7 @@ for ($i = 0; $i < GRID_X; $i++) {
         TAB . 'width: ' . ((100 / GRID_X) * ($i + 1)) . '%;' . PHP_EOL .
         '}' . PHP_EOL;
     $grid .= $style;
-}
+}*/
 
 //.col
 for ($i = 0; $i < GRID_X; $i++) {
@@ -42,11 +43,48 @@ for ($i = 0; $i < GRID_X; $i++) {
         $left = (100 / GRID_X) * (float)$i;
         $left = number_format($left, 5);
     }
-    $style = '.col-xs-' . ($i + 1) . ' {' . PHP_EOL .
+    $style = '.col-xxs-' . ($i + 1) . ' {' . PHP_EOL .
         //TAB . 'left: ' . $left . '%;' . PHP_EOL .
         TAB . 'width: ' . ((100 / GRID_X) * ($i + 1)) . '%;' . PHP_EOL .
         '}' . PHP_EOL;
     $grid .= $style;
 }
+
+$style = '@media all and (min-width: 400px) {'. PHP_EOL;
+for ($i = 0; $i < GRID_X; $i++) {
+    $left = '';
+    if ($i == 0) {
+        $left = 0;
+    } else {
+        $left = (100 / GRID_X) * (float)$i;
+        $left = number_format($left, 5);
+    }
+    $style .= TAB .'.col-xs-' . ($i + 1) . ' {' . PHP_EOL .
+        //TAB . 'left: ' . $left . '%;' . PHP_EOL .
+        TAB .TAB . 'width: ' . ((100 / GRID_X) * ($i + 1)) . '%;' . PHP_EOL .
+        TAB .'}' . PHP_EOL;
+    //$grid .= $style;
+}
+$style .= '}'. PHP_EOL;
+$grid .= $style;
+
+$style = '@media all and (min-width: 550px) {'. PHP_EOL;
+for ($i = 0; $i < GRID_X; $i++) {
+    $left = '';
+    if ($i == 0) {
+        $left = 0;
+    } else {
+        $left = (100 / GRID_X) * (float)$i;
+        $left = number_format($left, 5);
+    }
+    $style .= TAB .'.col-sm-' . ($i + 1) . ' {' . PHP_EOL .
+        //TAB . 'left: ' . $left . '%;' . PHP_EOL .
+        TAB .TAB . 'width: ' . ((100 / GRID_X) * ($i + 1)) . '%;' . PHP_EOL .
+        TAB .'}' . PHP_EOL;
+    //$grid .= $style;
+}
+$style .= '}'. PHP_EOL;
+$grid .= $style;
+
 
 file_put_contents(DIR . '/Public/Css/Partials/Grid.css', $grid);
