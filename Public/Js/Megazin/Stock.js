b0baf3e8f921ed2146e6e8985c686107621a0783
worzala <worzala86@gmail.com>
2018-10-12 20:33:02 +0200

    dodanie chart.js

--- Public/Js/Megazin/Stock.js
+++ Public/Js/Megazin/Stock.js
@@ -69,6 +69,36 @@ angular.module('Megazin')
             pagination.page = 1;
             loadPage()
         }
+        ////
+        $scope.labels = ["January", "February", "March", "April", "May", "June", "July"]
+        $scope.series = ['Series A'/*, 'Series B'*/]
+        $scope.data = [
+            [65, 59, 80, 81, 56, 55, 40],
+            //[28, 48, 40, 19, 86, 27, 90]
+        ]
+        $scope.onClick = function (points, evt) {
+            //console.log(points, evt)
+        }
+        $scope.datasetOverride = [{yAxisID: 'y-axis-1'}/*, {yAxisID: 'y-axis-2'}*/]
+        $scope.options = {
+            scales: {
+                yAxes: [
+                    {
+                        id: 'y-axis-1',
+                        type: 'linear',
+                        display: true,
+                        position: 'left'
+                    }/*,
+                    {
+                        id: 'y-axis-2',
+                        type: 'linear',
+                        display: true,
+                        position: 'right'
+                    }*/
+                ]
+            }
+        }
+        $scope.colors = ['rgba(255,255,255,0.2)'];////
     })
 
     .factory('stocks', function ($http, $httpParamSerializerJQLike) {
