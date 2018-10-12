30cd3e51647fa4c526628c47105cb80d257fc343
worzala <worzala86@gmail.com>
2018-10-12 21:54:34 +0200

    dodanie dashboard

--- Public/Js/Megazin.js
+++ Public/Js/Megazin.js
@@ -217,6 +217,11 @@ angular.module('Megazin', ['ngRoute', 'ui.tree', 'ngFileUpload', 'chart.js'])
                 controller: 'documentationController',
                 pageName: null,
             })
+            .when(base + '/panel', {
+                templateUrl: templateBase + 'Dashboard.html',
+                controller: 'dashboardController',
+                pageName: null,
+            })
         ;
 
         $locationProvider.html5Mode({
@@ -306,6 +311,39 @@ angular.module('Megazin', ['ngRoute', 'ui.tree', 'ngFileUpload', 'chart.js'])
     })
 
     .controller('landingController', function ($scope) {
+    })
+
+    .controller('dashboardController', function ($scope) {
+        ////
+        $scope.labels = ["January", "February", "March", "April", "May", "June", "July"]
+        $scope.series = ['Series A'/*, 'Series B'*/]
+        $scope.data = [
+            [65, 59, 80, 81, 56, 55, 40],
+            //[28, 48, 40, 19, 86, 27, 90]
+        ]
+        $scope.onClick = function (points, evt) {
+            //console.log(points, evt)
+        }
+        $scope.datasetOverride = [{yAxisID: 'y-axis-1'}/*, {yAxisID: 'y-axis-2'}*/]
+        $scope.options = {
+            scales: {
+                yAxes: [
+                    {
+                        id: 'y-axis-1',
+                        type: 'linear',
+                        display: true,
+                        position: 'left'
+                    }/*,
+                    {
+                        id: 'y-axis-2',
+                        type: 'linear',
+                        display: true,
+                        position: 'right'
+                    }*/
+                ]
+            }
+        }
+        $scope.colors = ['rgba(255,255,255,0.2)'];////
     })
 
     .controller('documentationController', function ($scope, $http) {
