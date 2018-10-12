67a8cb2d9983060b24bf4f746127ec2ce745a963
worzala <worzala86@gmail.com>
2018-10-12 12:58:19 +0200

    poprawki

--- Class/Module/User/Handler/UserStatusHandler.php
+++ Class/Module/User/Handler/UserStatusHandler.php
@@ -3,14 +3,14 @@
 namespace App\Module\User\Handler;
 
 use App\Handler;
+use App\Module\User\Request\UserStatusRequest;
 use App\Module\User\Response\UserStatusResponse;
-use App\Request\EmptyRequest;
 use App\User;
 
 class UserStatusHandler extends Handler
 {
 
-    public function __invoke(EmptyRequest $request): UserStatusResponse
+    public function __invoke(UserStatusRequest $request): UserStatusResponse
     {
         return (new UserStatusResponse)
             ->setLogged(User::getId()?true:false);
---
+++ Class/Module/User/Request/UserStatusRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Module\User\Request;
+
+use App\Request\Request;
+use App\Type\Mail;
+use App\Type\Password;
+
+class UserStatusRequest extends Request
+{
+}
